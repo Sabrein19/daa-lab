d09fddf203da0cb38b3a3fa8edf8bfab3ecd2609
Sabrein <sabrein1907@gmail.com>
2026-03-25 15:48:28 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 int count;
-void bubble_sort(int A[10],int n)
+void merge(int A[10],int l,int m,int r)
 {
-    int i,j,temp;
-    for(i=0;i<=n-2;i++)
+    int i,j,k,B[10];
+    i=l;
+    j=m+1;
+    k=l;
+    while(i<=m && j<=r)
     {
-         for(j=0;j<n-1-i;j++)
-         {
-             count=count+1;
-             if(A[j]>A[j+1])
-             {
-                 temp=A[j];
-                 A[j]=A[j+1];
-                 A[j+1]=temp;
-             }
-         }
+        count++;
+        i++;
+    }
+    while(i<=m)
+        B[k++]=A[i++];
+    while(j<=r)
+        B[k++]=A[j++];
+    for(i=l;i<=r;i++)
+        A[i]=B[i];
+}
+void merge_Sort(int A[10],int l,int r)
+{
+    int m;
+    if(l<r)
+    {
+        m=(l+r)/2;
+        merge_sort(A,l,m);
+        merge_sort(A,m+1,r);
+        merge(A,l,m,r);
     }
 }
 int main()
@@ -26,10 +38,11 @@ int main()
     printf("\nRead array elements:\n");
     for(i=0;i<n;i++)
         scanf("%d",&A[i]);
-    bubble_sort(A,n);
+    merge_sort(A,0,n-1);
     printf("\nSorted elements are:\n");
     for(i=0;i<n;i++)
         printf("%d\t",A[i]);
     printf("\nTotal number of comparisons= %d",count);
     return 0;
 }
+
