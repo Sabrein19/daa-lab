c1118e88064c7edc9a6d80deb193d6122d4e4875
Sabrein <sabrein1907@gmail.com>
2026-03-25 15:49:02 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,62 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
-int count;
-int partition(int A[10], int l, int r)
+#include <stdio.h>
+#include <stdlib.h>
+void bfs(int a[10][10],int n,int visited[10],int source)
 {
-    int pivot,i,j,temp;
-    pivot=A[l];
-    i=l+1;
-    j=r;
-    while(1)
+    int queue[10],front=0,rear=-1,i,delnode;
+    visited[source]=1;
+    queue[++rear]=source;
+    while(front<=rear)
     {
-        while(pivot>=A[i] && i<=r)
+        delnode=queue[front++];
+        for(i=0;i<n;i++)
         {
-            count++;
-            i++;
+            if(a[delnode][i]==1&&visited[i]==0)
+            {
+                visited[i]=1;
+                queue[++rear]=i;
+            }
         }
-        while(pivot<A[j])
-        {
-            count++;
-            j--;
-        }
-        if(i<j)
-        {
-            temp=A[i];
-            A[i]=A[j];
-            A[j]=temp;
-        }
-        else
-        {
-            temp=A[l];
-            A[l]=A[j];
-            A[j]=temp;
-            return j;
-        }
-    }
-}
-
-void quicksort(int A[10],int l,int r)
-{
-    int s;
-    if(l<r)
-    {
-        s=partition(A,l,r);
-        quicksort(A,l,s-1);
-        quicksort(A,s+l,r);
     }
 }
 int main()
 {
-    int i,n,A[10];
-    printf("\nRead array size:");
-    scanf("%d",&n);
-    printf("\nRead array elements\n");
-    for(i=0;i<n;i++)
-        scanf("%d",&A[i]);
-    quicksort(A,0,n-1);
-    printf("\nSorted elements are:\n");
-    for(i=0;i<n;i++)
-        printf("%d\t",A[i]);
-    printf("\nTotal no. of comparison=%d\n",count);
-    return 0;
+ int n,a[10][10],i,j,visited[10],count=0;
+ printf("\n read number of nodes:");
+ scanf("%d",&n);
+ printf("\n read adjacency matrix:\n");
+ for(i=0;i<n;i++)
+    for(j=0;j<n;j++)
+      scanf("%d",&a[i][j]);
+ for(i=0;i<n;i++)
+    visited[i]=0;
+ for(i=0;i<n;i++)
+ {
+     if(visited[i]==0)
+     {
+         bfs(a,n,visited,i);
+         count++;
+     }
+ }
+ if(count==1)
+    printf("\n graph is connected");
+else
+    printf("\n graph not connected & has %d components",count);
+return 0;
 }
